a8048eae1f5a55aa72d22a60409aa032a73b3a4f
Michael Polzin <polzin13@gmail.com>
2013-04-23 13:14:36 -0500

    Added friends COM name, changed file names

--- SeniorProjectService/SeniorProjectService/SeniorProject.cs
+++ SeniorProjectService/SeniorProjectService/SeniorProject.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace COM_Test
+namespace SeniorProjectService
 {
     class Program
     {
@@ -24,13 +24,7 @@ namespace COM_Test
 
             // Allow the user to set the appropriate properties.
             _serialPort.PortName = SetPortName(_serialPort.PortName);
-            _serialPort.BaudRate = SetPortBaudRate(_serialPort.BaudRate);
-            _serialPort.Parity = SetPortParity(_serialPort.Parity);
-            _serialPort.DataBits = SetPortDataBits(_serialPort.DataBits);
-            _serialPort.StopBits = SetPortStopBits(_serialPort.StopBits);
-            _serialPort.Handshake = SetPortHandshake(_serialPort.Handshake);
             _serialPort.Encoding = Encoding.UTF8;
-
 
             // Set the read/write timeouts
             _serialPort.ReadTimeout = 1500;
@@ -138,9 +132,9 @@ namespace COM_Test
             string portName;
 
             Console.WriteLine("Available Ports:");
-            foreach (string s in SerialPort.GetPortNames())
+            foreach (COMPortInfo comPort in COMPortInfo.GetCOMPortsInfo())
             {
-                Console.WriteLine("   {0}", s);
+                Console.WriteLine(string.Format("{0} – {1}", comPort.Name, comPort.Description));
             }
 
             Console.Write("COM port({0}): ", defaultPortName);
@@ -151,99 +145,6 @@ namespace COM_Test
                 portName = defaultPortName;
             }
             return portName;
-        }
-
-        public static int SetPortBaudRate(int defaultPortBaudRate)
-        {
-            string baudRate;
-
-            Console.Write("Baud Rate({0}): ", defaultPortBaudRate);
-            baudRate = Console.ReadLine();
-
-            if (baudRate == "")
-            {
-                baudRate = defaultPortBaudRate.ToString();
-            }
-
-            return int.Parse(baudRate);
-        }
-
-        public static Parity SetPortParity(Parity defaultPortParity)
-        {
-            string parity;
-
-            Console.WriteLine("Available Parity options:");
-            foreach (string s in Enum.GetNames(typeof(Parity)))
-            {
-                Console.WriteLine("   {0}", s);
-            }
-
-            Console.Write("Parity({0}):", defaultPortParity.ToString());
-            parity = Console.ReadLine();
-
-            if (parity == "")
-            {
-                parity = defaultPortParity.ToString();
-            }
-
-            return (Parity)Enum.Parse(typeof(Parity), parity);
-        }
-
-        public static int SetPortDataBits(int defaultPortDataBits)
-        {
-            string dataBits;
-
-            Console.Write("Data Bits({0}): ", defaultPortDataBits);
-            dataBits = Console.ReadLine();
-
-            if (dataBits == "")
-            {
-                dataBits = defaultPortDataBits.ToString();
-            }
-
-            return int.Parse(dataBits);
-        }
-
-        public static StopBits SetPortStopBits(StopBits defaultPortStopBits)
-        {
-            string stopBits;
-
-            Console.WriteLine("Available Stop Bits options:");
-            foreach (string s in Enum.GetNames(typeof(StopBits)))
-            {
-                Console.WriteLine("   {0}", s);
-            }
-
-            Console.Write("Stop Bits({0}):", defaultPortStopBits.ToString());
-            stopBits = Console.ReadLine();
-
-            if (stopBits == "")
-            {
-                stopBits = defaultPortStopBits.ToString();
-            }
-
-            return (StopBits)Enum.Parse(typeof(StopBits), stopBits);
-        }
-
-        public static Handshake SetPortHandshake(Handshake defaultPortHandshake)
-        {
-            string handshake;
-
-            Console.WriteLine("Available Handshake options:");
-            foreach (string s in Enum.GetNames(typeof(Handshake)))
-            {
-                Console.WriteLine("   {0}", s);
-            }
-
-            Console.Write("Handshake({0}):", defaultPortHandshake.ToString());
-            handshake = Console.ReadLine();
-
-            if (handshake == "")
-            {
-                handshake = defaultPortHandshake.ToString();
-            }
-
-            return (Handshake)Enum.Parse(typeof(Handshake), handshake);
         }
     }
 }
